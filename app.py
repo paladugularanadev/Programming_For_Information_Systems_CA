3903e398b4966029ecd3ac9dcb2e0737b28abc5c
paladugularanadev <108921089+paladugularanadev@users.noreply.github.com>
2023-05-16 12:41:54 +0100

    added product images
    added product images

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ def index():
     shopLen = len(shoppingCart)
     totItems, total, display = 0, 0, 0
     if 'user' in session:
-        shoppingCart = db.execute("SELECT team, image, SUM(qty), SUM(subTotal), price, id FROM cart GROUP BY team")
+        shoppingCart = db.execute("SELECT product, image, SUM(qty), SUM(subTotal), price, id FROM cart GROUP BY product")
         shopLen = len(shoppingCart)
         for i in range(shopLen):
             total += shoppingCart[i]["SUM(subTotal)"]
             totItems += shoppingCart[i]["SUM(qty)"]
-        products = db.execute("SELECT * FROM products ORDER BY team ASC")
+        products = db.execute("SELECT * FROM products ORDER BY product ASC")
         productsLen = len(products)
         return render_template ("index.html", shoppingCart=shoppingCart, products=products, shopLen=shopLen, productsLen=productsLen, total=total, totItems=totItems, display=display, session=session )
     return render_template ( "index.html", products=products, shoppingCart=shoppingCart, productsLen=productsLen, shopLen=shopLen, total=total, totItems=totItems, display=display)
